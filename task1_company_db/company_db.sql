1deedb210118fa9cf24f3e4fca7ff3ef132c2e0b
Mayur Satpaise <mayurbro77@gmail.com>
2025-07-04 12:20:45 +0530

    update_sql

---
+++ task1_company_db/company_db.sql
@@ -0,0 +1,22 @@
+select * from client;
+
+copy client from 'C:/git_hub_desktop/SQL TASK/task1_company_db/2_july_2025_client_company_db.csv' delimiter ',' csv header;
+
+select * from departments;
+
+copy departments from 'C:/git_hub_desktop/SQL TASK/task1_company_db/2_july_2025_departments_company_db.csv' delimiter ',' csv header;
+
+select * from departments;
+
+select * from employees;
+
+copy employees from 'C:/git_hub_desktop/SQL TASK/task1_company_db/2_july_2025_employees_company_db.csv' delimiter ',' csv header;
+
+select * from products;
+
+copy products from 'C:/git_hub_desktop/SQL TASK/task1_company_db/2_july_2025_products_company_db.csv' delimiter ',' csv header;
+
+select * from projects;
+
+copy projects from 'C:/git_hub_desktop/SQL TASK/task1_company_db/2_july_projects_company_db.csv' delimiter ',' csv header;
+
